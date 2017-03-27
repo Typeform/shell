93586bb4b75d342421c6188be933700693acd6e5
Terrence Wong <wongterrencew@gmail.com>
2017-03-27 14:13:46 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="app" className="app">
+        <div className="shell">
+          <div className="shell__output"></div>
+          <div className="shell__input-line">
+            <span className="shell__prompt">></span>
+            <input id="shell-input" className="shell__input" />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
